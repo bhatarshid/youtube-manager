b8c99296cb2b0b924fd588d66e4a9f6d81045930
arshid.bhat <arshid.bhat@hexaviewtech.com>
2023-12-27 11:41:15 +0530

    some tweaks

--- src/controllers/upload-video-controller.ts
+++ src/controllers/upload-video-controller.ts
@@ -5,7 +5,7 @@ import { uploadVideoOnYoutubeService } from '../services/upload-video-service'
 
 export const uploadVideoOnYoutube = async (req: express.Request, res: express.Response) => {
     try {
-        const response = await uploadVideoOnYoutubeService(req.body, req.file);
+        const response = await uploadVideoOnYoutubeService(req.body, req.file.buffer);
         return res.send(response);
     }
     catch (error) {
--- src/services/upload-video-service.ts
+++ src/services/upload-video-service.ts
@@ -3,18 +3,16 @@ import { google } from 'googleapis';
 
 
 //change into async await
-export async function uploadVideoOnYoutubeService(reqObj: Record<string, any>, videoFile: any) {
+export async function uploadVideoOnYoutubeService(reqObj: Record<string, any>, videoFile: Buffer) {
     try {
         let auth = await authorize();
         reqObj.auth= auth
-        reqObj.media= {
-            body: videoFile.buffer
-        }
         
         const SERVICE = google.youtube('v3');
         const response = await SERVICE.videos.insert(reqObj)
 
         if(response.status != 200) throw new Error('Something went wrong while uploading the video')
+
         return 'Video uploaded'
     }
     catch (error) {
