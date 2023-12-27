f03f95041047df76cc594224b5ef9ef11c2fbbe6
arshid.bhat <arshid.bhat@hexaviewtech.com>
2023-12-27 16:05:12 +0530

    minor-tweak

--- src/config/db-connect.ts
+++ src/config/db-connect.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connectDb(){
-    mongoose.connect(process.env.MONGODB_URL);
+    mongoose.connect(process.env.MONGODB_URL + process.env.DB_NAME);
     mongoose.connection.on('error', (error: Error) => console.log('Error while connecting to database' + error));
 }
 
