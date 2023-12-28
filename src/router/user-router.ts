a7e89c644bbf318acbcecd74d7466c2b992398df
Arshid <60290121+bhatarshid@users.noreply.github.com>
2023-12-28 23:26:22 +0530

    Revert "Create user"

--- src/router/user-router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-import { createNewUser, getUserById } from '../controllers/user-controller';
-
-export default (router: express.Router) => {
-    router.post('/user', createNewUser);
-    router.get('/user/:id', getUserById);
-}
